ae1bc90f633e4691f9df8d3a8ba318f7069c40e6
Travis Davis <skoolboi434@gmail.com>
2023-11-09 23:49:48 -0500

    Login / Logout functionality added

--- app/Main.js
+++ app/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
@@ -6,13 +6,15 @@ import Footer from './components/Footer';
 import HomeGuests from './pages/HomeGuests';
 import AboutPage from './pages/AboutPage';
 import TermsPage from './pages/TermsPage';
+import Home from './pages/Home';
 
 function Main() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('complexappToken')));
   return (
     <BrowserRouter>
-      <Header />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
-        <Route path='/' exact={true} element={<HomeGuests />} />
+        <Route path='/' exact={true} element={loggedIn ? <Home /> : <HomeGuests />} />
         <Route path='/about-us' element={<AboutPage />} />
         <Route path='/terms' element={<TermsPage />} />
       </Routes>
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import HeaderLoggedOut from './HeaderLoggedOut';
+import HeaderLoggedIn from './HeaderLoggedIn';
 
-const Header = () => {
+const Header = props => {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('complexappToken')));
+
   return (
     <>
       <header className='header-bar bg-primary mb-3'>
@@ -12,19 +16,7 @@ const Header = () => {
               ComplexApp{' '}
             </Link>
           </h4>
-          <form className='mb-0 pt-2 pt-md-0'>
-            <div className='row align-items-center'>
-              <div className='col-md mr-0 pr-md-0 mb-3 mb-md-0'>
-                <input name='username' className='form-control form-control-sm input-dark' type='text' placeholder='Username' autoComplete='off' />
-              </div>
-              <div className='col-md mr-0 pr-md-0 mb-3 mb-md-0'>
-                <input name='password' className='form-control form-control-sm input-dark' type='password' placeholder='Password' />
-              </div>
-              <div className='col-md-auto'>
-                <button className='btn btn-success btn-sm'>Sign In</button>
-              </div>
-            </div>
-          </form>
+          {props.loggedIn ? <HeaderLoggedIn setLoggedIn={props.setLoggedIn} /> : <HeaderLoggedOut setLoggedIn={setLoggedIn} />}
         </div>
       </header>
     </>

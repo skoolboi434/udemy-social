3a71104b0f4cf638c2a6e8cc57752e7539e26724
Travis Davis <skoolboi434@gmail.com>
2023-11-10 13:11:44 -0500

    Frontend and backend connected

--- app/Main.js
+++ app/Main.js
@@ -1,3 +1,4 @@
+import Axios from 'axios';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -7,16 +8,32 @@ import HomeGuests from './pages/HomeGuests';
 import AboutPage from './pages/AboutPage';
 import TermsPage from './pages/TermsPage';
 import Home from './pages/Home';
+import CreatePost from './pages/CreatePost';
+import SinglePost from './pages/SinglePost';
+import FlashMessages from './components/FlashMessages';
+
+Axios.defaults.baseURL = 'http://localhost:8080';
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('complexappToken')));
+
+  const [flashMessages, setFlashMessages] = useState([]);
+
+  const addFlashMessage = msg => {
+    setFlashMessages(prev => prev.concat(msg));
+  };
+
   return (
     <BrowserRouter>
+      <FlashMessages messages={flashMessages} />
       <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+
       <Routes>
         <Route path='/' exact={true} element={loggedIn ? <Home /> : <HomeGuests />} />
         <Route path='/about-us' element={<AboutPage />} />
         <Route path='/terms' element={<TermsPage />} />
+        <Route path='/create-post' element={<CreatePost addFlashMessage={addFlashMessage} />} />
+        <Route path='/post/:id' element={<SinglePost />} />
       </Routes>
 
       <Footer />
---
+++ app/pages/CreatePost.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Page from '../components/Page';
+import Axios from 'axios';
+
+const CreatePost = props => {
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+
+  const navgiate = useNavigate();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const res = await Axios.post('/create-post', {
+        title,
+        body,
+        token: localStorage.getItem('complexappToken')
+      });
+      props.addFlashMessage('Congrats, you successfully created a post.');
+      // Redirect To New Post URL
+      navgiate(`/post/${res.data}`);
+    } catch (error) {
+      console.log(`There was a problem: ${error}`);
+    }
+  };
+  return (
+    <Page title='Create New Post'>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='post-title' className='text-muted mb-1'>
+            <small>Title</small>
+          </label>
+          <input onChange={e => setTitle(e.target.value)} autoFocus name='title' id='post-title' className='form-control form-control-lg form-control-title' type='text' placeholder='' autocomplete='off' />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='post-body' className='text-muted mb-1 d-block'>
+            <small>Body Content</small>
+          </label>
+          <textarea onChange={e => setBody(e.target.value)} name='body' id='post-body' className='body-content tall-textarea form-control' type='text'></textarea>
+        </div>
+
+        <button className='btn btn-primary'>Save New Post</button>
+      </form>
+    </Page>
+  );
+};
+
+export default CreatePost;
